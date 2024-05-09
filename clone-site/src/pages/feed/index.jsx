8df662cf41b6ea3e5f01a931fe48b9f0d8114cf7
lucasbizachi <bizachilucas@gmail.com>
2024-05-09 15:12:30 -0300

    estilizando página logada

--- clone-site/src/pages/feed/index.jsx
+++ clone-site/src/pages/feed/index.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import bannerImage from "../../assets/banner.svg";
 import { Card } from "../../components/Card"
+import { UserInfo } from '../../components/UserInfo';
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header"
 import {Container, TextContent, Title, TitleHighLight} from './styles'
+
 
 const Feed = () => {
     return(<>
         <Header />
         <Container>
             <Card />
+            <UserInfo percentual={80} nome="Usuário"  
+            image="https://avatars.githubusercontent.com/u/101759223?v=4"/>
         </Container>
     </>)
 }
