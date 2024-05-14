b028ebd39601da2334e064a29e8589ddec9f9294
lucasbizachi <bizachilucas@gmail.com>
2024-05-14 11:50:13 -0300

    colocando rotas e importando axios

--- clone-site/src/pages/login/index.jsx
+++ clone-site/src/pages/login/index.jsx
@@ -2,12 +2,14 @@ import { MdEmail, MdLock } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup  from "yup";
+import * as yup from "yup";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
-import {Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper} from './styles'
+
+import { api } from '../../services/api';
+import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
 
 const schema = yup.object({
     email: yup.string().email('Email não é válido.').required('Campo obrigatório'),
@@ -18,46 +20,55 @@ const Login = () => {
 
     const navigate = useNavigate();
 
-        const { control, handleSubmit, formState: { errors, isValid } } = useForm({
-            resolver: yupResolver(schema),
-            mode:'onChange',
-        });
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+    });
 
-        console.log(isValid, errors);
-           
+    const onSubmit = async formData => {
+        try {
+            const { data } = await api.post('/login', {
+                email: formData.email,
+                senha: formData.password
+            });
+            if (data.length === 1) {
+                navigate('/feed');
+            } else {
+                alert('Email ou senha inválido.');
+            }
+        } catch {
+            alert('Houve um erro, tente novamente.');
+        }
+    };
 
-        const onsubmit = data => console.log(data);
-
-        const handleClickSignIn = () => {
-            navigate('/feed')
-        
-    }
-
-    return(<>
-        <Header />
-        <Container>
-            <Column>
-                <Title>
-                    A plataforma pra você aprender com experts, dominar as principais tecnologias
-                    e entrar mais rápido nas empresas desejadas.
-                </Title>
-            </Column>
-            <Column>
-                <Wrapper>
-                    <TitleLogin>Faça seu cadastro</TitleLogin>
-                    <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                    <form onSubmit={handleSubmit(onsubmit)}>
-                        <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="Email" leftIcon={<MdEmail />}/>
-                        <Input name="password" errorMessage={errors?.password?.message} control={control}  placeholder="Senha" type="password" leftIcon={<MdLock />}/>
-                        <Button title="Entrar" variant="secondary" type="submit"/>
-                    </form>
-                    <Row>
-                        <EsqueciText>Esqueci minha senha</EsqueciText>
-                        <CriarText>Criar conta</CriarText>
-                    </Row>
-                </Wrapper>
-            </Column>
-        </Container>
-    </>)
+    return (
+        <>
+            <Header />
+            <Container>
+                <Column>
+                    <Title>
+                        A plataforma pra você aprender com experts, dominar as principais tecnologias
+                        e entrar mais rápido nas empresas desejadas.
+                    </Title>
+                </Column>
+                <Column>
+                    <Wrapper>
+                        <TitleLogin>Faça seu login</TitleLogin>
+                        <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="Email" leftIcon={<MdEmail />} />
+                            <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password" leftIcon={<MdLock />} />
+                            <Button title="Entrar" variant="secondary" type="submit" />
+                        </form>
+                        <Row>
+                            <EsqueciText>Esqueci minha senha</EsqueciText>
+                            <CriarText>Criar conta</CriarText>
+                        </Row>
+                    </Wrapper>
+                </Column>
+            </Container>
+        </>
+    );
 }
-export { Login }
+
+export { Login };
